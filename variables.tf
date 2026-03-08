924ba424300e60d6a5d323c45f2eb87fc5a66bc9
Shrinith <shrinithnhebbar4255@gmail.com>
2026-03-08 12:33:33 +0530

    Initial Terraform AWS setup

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "ap-south-1"
+}
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
