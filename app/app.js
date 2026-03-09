f01fcf2f656246df23a82df1d74fe4eee70eb509
Shrinith <shrinithnhebbar4255@gmail.com>
2026-03-09 15:16:57 +0530

    Add URL shortener UI with copy button and in-memory storage

--- app/app.js
+++ app/app.js
@@ -1,10 +1,55 @@
-const http = require("http");
+const express = require("express");
+const crypto = require("crypto");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from AWS ECS with Terraform!");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const urlDatabase = {};
+
+function generateCode() {
+  return crypto.randomBytes(3).toString("hex");
+}
+
+app.post("/shorten", (req, res) => {
+
+  const originalUrl = req.body.url;
+  const code = generateCode();
+
+  urlDatabase[code] = {
+    original: originalUrl,
+    clicks: 0
+  };
+
+  const shortUrl = `${req.protocol}://${req.get("host")}/${code}`;
+
+  res.json({
+    shortUrl: shortUrl
+  });
+
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.get("/:code", (req, res) => {
+
+  const code = req.params.code;
+
+  if (urlDatabase[code]) {
+
+    urlDatabase[code].clicks++;
+
+    res.redirect(urlDatabase[code].original);
+
+  } else {
+
+    res.status(404).send("URL not found");
+
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
